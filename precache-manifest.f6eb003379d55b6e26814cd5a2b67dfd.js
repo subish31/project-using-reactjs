6bf5009a3a03bfb1a8bff59d42a6eb6e7cf3fae5
subish31 <subishkumar31@gmial.com>
2024-08-14 15:31:34 +0530

    Updates

---
+++ precache-manifest.f6eb003379d55b6e26814cd5a2b67dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfa55922d1e832c22eadc904be97adb",
+    "url": "/subish31/head-content-footer-static-website-using-react/index.html"
+  },
+  {
+    "revision": "3467495691456385aca6",
+    "url": "/subish31/head-content-footer-static-website-using-react/static/css/main.034f6bdb.chunk.css"
+  },
+  {
+    "revision": "3f7606ca3cc508ccdaff",
+    "url": "/subish31/head-content-footer-static-website-using-react/static/js/2.44e7fc68.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/subish31/head-content-footer-static-website-using-react/static/js/2.44e7fc68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3467495691456385aca6",
+    "url": "/subish31/head-content-footer-static-website-using-react/static/js/main.52f9380a.chunk.js"
+  },
+  {
+    "revision": "0d8d598666752a78ce1c",
+    "url": "/subish31/head-content-footer-static-website-using-react/static/js/runtime-main.3e558c68.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-using-reactjs/precache-manifest.795957802842a557e9725d2c7b60af7d.js"
+  "/subish31/head-content-footer-static-website-using-react/precache-manifest.f6eb003379d55b6e26814cd5a2b67dfd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-using-reactjs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/subish31/head-content-footer-static-website-using-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
