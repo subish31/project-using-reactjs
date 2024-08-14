87b32cf5dceae2b7b77867b3bcfabc24c04e76f8
subish31 <subishkumar31@gmial.com>
2024-08-14 15:28:06 +0530

    Updates

---
+++ precache-manifest.795957802842a557e9725d2c7b60af7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc9f959b367a061a178a6880db872b2",
+    "url": "/project-using-reactjs/index.html"
+  },
+  {
+    "revision": "3467495691456385aca6",
+    "url": "/project-using-reactjs/static/css/main.034f6bdb.chunk.css"
+  },
+  {
+    "revision": "3f7606ca3cc508ccdaff",
+    "url": "/project-using-reactjs/static/js/2.44e7fc68.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/project-using-reactjs/static/js/2.44e7fc68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3467495691456385aca6",
+    "url": "/project-using-reactjs/static/js/main.52f9380a.chunk.js"
+  },
+  {
+    "revision": "d9c437d802acd5ab61d8",
+    "url": "/project-using-reactjs/static/js/runtime-main.5428ceb2.js"
+  }
+]);
